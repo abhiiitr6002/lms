84548ea9f1b70e2f2d9d181d6e147cfb681aedb1
Abhishek <abhiiitr6002@gmail.com>
2017-10-26 17:26:25 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class B{
 	public static void main(String [] args){
 		System.out.println("Hellllllllooooooooooooooo");
-                System.out.println("from the other side");
+                System.out.println("from the other ");
 	}
 }
